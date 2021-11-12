330947ae4b60210609f48b2d3ffbcc7a44bace88
Yohann PERRON <yohann.perron@polytechnique.edu>
2021-11-12 18:41:32 +0100

    working grouping

--- pointnet2/grouping.py
+++ pointnet2/grouping.py
@@ -1,50 +1,48 @@
+from hashlib import new
 import tensorflow as tf
 from tensorflow.python.framework import ops
 import sys
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(BASE_DIR)
+from tensorflow.python.ops.sort_ops import sort
 
 
-grouping_module=tf.load_op_library(os.path.join(BASE_DIR, 'tf_grouping_so.so'))
+def grouping(radius, nsample,new_xyz, xyz, points):
+    '''
+    Input:
+        radius: float
+        nsample: int
+        new_xyz: (batch_size, npoint, 3)
+        xyz: (batch_size, ndataset, 3) TF tensor
+        points: (batch_size, ndataset, channel) TF tensor, if None will just use xyz as points
+    Output:
+        new_points: (batch_size, npoint, nsample, 3+channel) TF tensor
+        idx: (batch_size, npoint, nsample) TF tensor, indices of local points as in ndataset points
+        grouped_xyz: (batch_size, npoint, nsample, 3) TF tensor, normalized point XYZs
+            (subtracted by seed point XYZ) in local regions
+    '''
+    batch_size,ndataset,__ = xyz.shape
+    npoint = new_xyz.shape[1]
 
-def query_ball_point(radius, nsample, xyz1, xyz2):
-    '''
-    Input:
-        radius: float32, ball search radius
-        nsample: int32, number of points selected in each ball region
-        xyz1: (batch_size, ndataset, 3) float32 array, input points
-        xyz2: (batch_size, npoint, 3) float32 array, query points
-    Output:
-        idx: (batch_size, npoint, nsample) int32 array, indices to input points
-        pts_cnt: (batch_size, npoint) int32 array, number of unique points in each local region
-    '''
-    #return grouping_module.query_ball_point(radius, nsample, xyz1, xyz2)
-    return grouping_module.query_ball_point(xyz1, xyz2, radius, nsample)
-ops.NoGradient('QueryBallPoint')
-def select_top_k(k, dist):
-    '''
-    Input:
-        k: int32, number of k SMALLEST elements selected
-        dist: (b,m,n) float32 array, distance matrix, m query points, n dataset points
-    Output:
-        idx: (b,m,n) int32 array, first k in n are indices to the top k
-        dist_out: (b,m,n) float32 array, first k in n are the top k
-    '''
-    return grouping_module.selection_sort(dist, k)
-ops.NoGradient('SelectionSort')
-def group_point(points, idx):
-    '''
-    Input:
-        points: (batch_size, ndataset, channel) float32 array, points to sample from
-        idx: (batch_size, npoint, nsample) int32 array, indices to points
-    Output:
-        out: (batch_size, npoint, nsample, channel) float32 array, values sampled from points
-    '''
-    return grouping_module.group_point(points, idx)
-@tf.RegisterGradient('GroupPoint')
-def _group_point_grad(op, grad_out):
-    points = op.inputs[0]
-    idx = op.inputs[1]
-    return [grouping_module.group_point_grad(points, idx, grad_out), None]
+    idx = tf.Variable(tf.zeros([batch_size, npoint, nsample], dtype=tf.int32))
+
+    for centre in range(npoint):
+        delta = xyz-tf.reshape(new_xyz[:,centre,:],[batch_size, 1, 3])  #shape: batch_size, ndataset, 3
+        dist = tf.norm(delta, axis = -1)                                #shape: batch_size, ndataset
+        sorted_indices = tf.argsort(dist, axis=-1)
+        sorted = tf.gather(dist, sorted_indices, batch_dims=-1)
+        centroid_indices = sorted_indices[:,0]
+        y = tf.broadcast_to(centroid_indices[:, tf.newaxis],sorted_indices.shape)   #shape: batch_size, ndataset
+        radius_indices = tf.where(sorted<radius, sorted_indices, y)     #shape: batch_size, ndataset
+        if ndataset<nsample:
+            z = tf.broadcast_to(centroid_indices[:, tf.newaxis],[batch_size, nsample-ndataset])
+            radius_indices = tf.concat([radius_indices,z], axis=-1)
+        idx[:,centre,:].assign(radius_indices[:,:nsample])
+    
+    grouped_xyz = tf.gather(xyz, idx, batch_dims=1)
+    if points == None:
+         return grouped_xyz, idx, grouped_xyz
+    else:
+        grouped_points = tf.gather(points, idx, batch_dims=1)
+        new_points = tf.concat([grouped_xyz,grouped_points], axis=-1)
+        return new_points, idx, grouped_xyz
