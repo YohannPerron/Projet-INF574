3a6943abac0851a8d99eae5ef16b2879e897607c
georgiohelou <42733892+georgiohelou@users.noreply.github.com>
2021-11-12 16:06:36 +0100

    Update grouping.py

---
+++ pointnet2/grouping.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from tensorflow.python.framework import ops
+import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+
+
+grouping_module=tf.load_op_library(os.path.join(BASE_DIR, 'tf_grouping_so.so'))
+
+def query_ball_point(radius, nsample, xyz1, xyz2):
+    '''
+    Input:
+        radius: float32, ball search radius
+        nsample: int32, number of points selected in each ball region
+        xyz1: (batch_size, ndataset, 3) float32 array, input points
+        xyz2: (batch_size, npoint, 3) float32 array, query points
+    Output:
+        idx: (batch_size, npoint, nsample) int32 array, indices to input points
+        pts_cnt: (batch_size, npoint) int32 array, number of unique points in each local region
+    '''
+    #return grouping_module.query_ball_point(radius, nsample, xyz1, xyz2)
+    return grouping_module.query_ball_point(xyz1, xyz2, radius, nsample)
+ops.NoGradient('QueryBallPoint')
+def select_top_k(k, dist):
+    '''
+    Input:
+        k: int32, number of k SMALLEST elements selected
+        dist: (b,m,n) float32 array, distance matrix, m query points, n dataset points
+    Output:
+        idx: (b,m,n) int32 array, first k in n are indices to the top k
+        dist_out: (b,m,n) float32 array, first k in n are the top k
+    '''
+    return grouping_module.selection_sort(dist, k)
+ops.NoGradient('SelectionSort')
+def group_point(points, idx):
+    '''
+    Input:
+        points: (batch_size, ndataset, channel) float32 array, points to sample from
+        idx: (batch_size, npoint, nsample) int32 array, indices to points
+    Output:
+        out: (batch_size, npoint, nsample, channel) float32 array, values sampled from points
+    '''
+    return grouping_module.group_point(points, idx)
+@tf.RegisterGradient('GroupPoint')
+def _group_point_grad(op, grad_out):
+    points = op.inputs[0]
+    idx = op.inputs[1]
+    return [grouping_module.group_point_grad(points, idx, grad_out), None]
