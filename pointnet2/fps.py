614272dcab3b6eabb2bc76f4d2a7419ef468a2fb
Yohann PERRON <yohann.perron@polytechnique.edu>
2021-11-12 17:32:38 +0100

    cleaning

--- pointnet2/fps.py
+++ pointnet2/fps.py
@@ -9,16 +9,15 @@ def fps(xyz, npoint):
     Output:
         new_xyz: (batch_size, npoint, 3) TF tensor
     '''
-    batch_size = xyz.shape[0]
-    ndataset = xyz.shape[1]
+    batch_size,ndataset,__ = xyz.shape
 
     new_indices = tf.Variable(tf.zeros([batch_size, npoint,2], dtype=tf.int32))
     for ex in range(batch_size):
         new_indices[ex, : , 0].assign(tf.fill([npoint], ex))
     dist = tf.zeros([batch_size,ndataset])
     for i in range(1,npoint):
-        diff = xyz-tf.reshape(tf.gather_nd(xyz, new_indices[:,i-1]),[batch_size,1,3])
-        dist_to_last = tf.norm(diff, axis = -1) # shape = (batch_size, ndataset)
+        delta = xyz-tf.reshape(tf.gather_nd(xyz, new_indices[:,i-1]),[batch_size,1,3])
+        dist_to_last = tf.norm(delta, axis = -1) # shape = (batch_size, ndataset)
         dist = tf.maximum(dist, dist_to_last)
         new_indices[:,i,1].assign(tf.argmax(dist, axis = -1, output_type=tf.int32)) # shape = (batch_size)
     return tf.gather_nd(xyz, new_indices)
