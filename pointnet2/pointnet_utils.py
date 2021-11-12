5b66fcaeb9d42f4e9c20c5c11ff30a0364b94fde
Yohann PERRON <yohann.perron@polytechnique.edu>
2021-11-12 18:42:05 +0100

    working sampling/grouping

--- pointnet2/pointnet_utils.py
+++ pointnet2/pointnet_utils.py
@@ -21,5 +21,17 @@ def sample_and_group(npoint, radius, nsample, xyz, points):
         grouped_xyz: (batch_size, npoint, nsample, 3) TF tensor, normalized point XYZs
             (subtracted by seed point XYZ) in local regions
     '''
-    new_xyz = fps(xyz)
+    new_xyz = fps.fps(xyz, npoint)
+    new_points, idx, grouped_xyz = grouping.grouping(radius, nsample,new_xyz, xyz, points)
+    return new_xyz, new_points, idx, grouped_xyz
+
+if __name__ == "__main__":
+    tf.compat.v1.enable_eager_execution()
+    xyz = [
+        [[1,0,0],[0,0,-10],[10,0,0],[10,5,0]],
+        [[-1,0,0],[0,100,0],[10,0,0],[10,0,7]]
+    ]
+    xyz = tf.convert_to_tensor(xyz, dtype=tf.float32)
+    new_xyz, new_points, idx, grouped_xyz = sample_and_group(2,40,3,xyz, xyz)
+    tf.print(new_points)
     
