f912d126fb5e567628f64c70e84e5bd033cc71a2
Yohann PERRON <yohann.perron@polytechnique.edu>
2021-11-21 16:10:09 +0100

    test

--- pointnet2_class.py
+++ pointnet2_class.py
@@ -13,21 +13,21 @@ def get_model(NUM_POINTS):
     xyz_0 = point_cloud
     points_0 = None
 
-    # downsampling
-    xyz_1, points_1, _ = pointnet_downsample(xyz_0, points_0, 512, 0.2, 32, [64,64,128])
-    xyz_2, points_2, _ = pointnet_downsample(xyz_1, points_1, 128, 0.4, 64, [128,128,256])
-    xyz_3, points_3, _ = pointnet_downsample(xyz_2, points_2, 1, np.inf, 128, [256,512,1024])
+    # # downsampling
+    # xyz_1, points_1, _ = pointnet_downsample(xyz_0, points_0, 512, 0.2, 32, [64,64,128])
+    # xyz_2, points_2, _ = pointnet_downsample(xyz_1, points_1, 128, 0.4, 64, [128,128,256])
+    # xyz_3, points_3, _ = pointnet_downsample(xyz_2, points_2, 1, np.inf, 128, [256,512,1024])
 
-    # Fully connected layers
-    net = tf.reshape(points_3, [batch_size, -1])
-    net= tf.keras.layers.Dense(512)(net)
-    net = tf.keras.layers.ReLU()(net)
-    net = tf.keras.layers.Dropout(0.5)(net)
-    net= tf.keras.layers.Dense(256)(net)
-    net = tf.keras.layers.ReLU()(net)
-    net = tf.keras.layers.Dropout(0.5)(net)
-    #change back to 40
-    net= tf.keras.layers.Dense(10)(net)
+    # # Fully connected layers
+    # net = tf.reshape(points_3, [batch_size, -1])
+    # net= tf.keras.layers.Dense(512)(net)
+    # net = tf.keras.layers.ReLU()(net)
+    # net = tf.keras.layers.Dropout(0.5)(net)
+    # net= tf.keras.layers.Dense(256)(net)
+    # net = tf.keras.layers.ReLU()(net)
+    # net = tf.keras.layers.Dropout(0.5)(net)
+    # #change back to 40
+    # net= tf.keras.layers.Dense(10)(net)
 
 
     return keras.Model(inputs=inputs, outputs=net, name="pointnet2")
