3f7b32704242709d0fc8a6880c719c1db3bd0606
Yohann PERRON <yohann.perron@polytechnique.edu>
2021-11-21 14:26:20 +0100

    Cleaning

--- train_cls.py
+++ train_cls.py
@@ -8,6 +8,12 @@ from tensorflow.keras import layers
 from matplotlib import pyplot as plt
 from pointnet2_class import get_model
 
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
+
+import sys
+sys.setrecursionlimit(4000)
+
 tf.random.set_seed(1234)
 
 """
@@ -79,6 +85,10 @@ P.S: change the number of classes to 40 for modelNet40
 NUM_POINTS = 2048
 NUM_CLASSES = 10
 BATCH_SIZE = 32
+
+model = get_model(BATCH_SIZE,NUM_POINTS)
+model.summary()
+
 """
 Parse data in tf.data.Dataset()
 """
@@ -109,108 +119,106 @@ test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
 train_dataset = train_dataset.shuffle(len(train_points)).map(augment).batch(BATCH_SIZE)
 test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
 
-"""
-### Build a model
-Each convolution and fully-connected layer (with exception for end layers) consits of
-Convolution / Dense -> Batch Normalization -> ReLU Activation.
-"""
+# """
+# ### Build a model
+# Each convolution and fully-connected layer (with exception for end layers) consits of
+# Convolution / Dense -> Batch Normalization -> ReLU Activation.
+# """
 
 
-def conv_bn(x, filters):
-    x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
-    x = layers.BatchNormalization(momentum=0.0)(x)
-    return layers.Activation("relu")(x)
+# def conv_bn(x, filters):
+#     x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
+#     x = layers.BatchNormalization(momentum=0.0)(x)
+#     return layers.Activation("relu")(x)
 
 
-def dense_bn(x, filters):
-    x = layers.Dense(filters)(x)
-    x = layers.BatchNormalization(momentum=0.0)(x)
-    return layers.Activation("relu")(x)
+# def dense_bn(x, filters):
+#     x = layers.Dense(filters)(x)
+#     x = layers.BatchNormalization(momentum=0.0)(x)
+#     return layers.Activation("relu")(x)
 
 
-"""
-PointNet consists of two core components. The primary MLP network, and the transformer
-net (T-net). The T-net aims to learn an affine transformation matrix by its own mini
-network. The T-net is used twice. The first time to transform the input features (n, 3)
-into a canonical representation. The second is an affine transformation for alignment in
-feature space (n, 3). As per the original paper we constrain the transformation to be
-close to an orthogonal matrix (i.e. ||X*X^T - I|| = 0).
-"""
+# """
+# PointNet consists of two core components. The primary MLP network, and the transformer
+# net (T-net). The T-net aims to learn an affine transformation matrix by its own mini
+# network. The T-net is used twice. The first time to transform the input features (n, 3)
+# into a canonical representation. The second is an affine transformation for alignment in
+# feature space (n, 3). As per the original paper we constrain the transformation to be
+# close to an orthogonal matrix (i.e. ||X*X^T - I|| = 0).
+# """
 
 
-class OrthogonalRegularizer(keras.regularizers.Regularizer):
-    def __init__(self, num_features, l2reg=0.001):
-        self.num_features = num_features
-        self.l2reg = l2reg
-        self.eye = tf.eye(num_features)
+# class OrthogonalRegularizer(keras.regularizers.Regularizer):
+#     def __init__(self, num_features, l2reg=0.001):
+#         self.num_features = num_features
+#         self.l2reg = l2reg
+#         self.eye = tf.eye(num_features)
 
-    def __call__(self, x):
-        x = tf.reshape(x, (-1, self.num_features, self.num_features))
-        xxt = tf.tensordot(x, x, axes=(2, 2))
-        xxt = tf.reshape(xxt, (-1, self.num_features, self.num_features))
-        return tf.reduce_sum(self.l2reg * tf.square(xxt - self.eye))
-
-
-"""
- We can then define a general function to build T-net layers.
-"""
+#     def __call__(self, x):
+#         x = tf.reshape(x, (-1, self.num_features, self.num_features))
+#         xxt = tf.tensordot(x, x, axes=(2, 2))
+#         xxt = tf.reshape(xxt, (-1, self.num_features, self.num_features))
+#         return tf.reduce_sum(self.l2reg * tf.square(xxt - self.eye))
 
 
-def tnet(inputs, num_features):
-
-    # Initalise bias as the indentity matrix
-    bias = keras.initializers.Constant(np.eye(num_features).flatten())
-    reg = OrthogonalRegularizer(num_features)
-
-    x = conv_bn(inputs, 32)
-    x = conv_bn(x, 64)
-    x = conv_bn(x, 512)
-    x = layers.GlobalMaxPooling1D()(x)
-    x = dense_bn(x, 256)
-    x = dense_bn(x, 128)
-    x = layers.Dense(
-        num_features * num_features,
-        kernel_initializer="zeros",
-        bias_initializer=bias,
-        activity_regularizer=reg,
-    )(x)
-    feat_T = layers.Reshape((num_features, num_features))(x)
-    # Apply affine transformation to input features
-    return layers.Dot(axes=(2, 1))([inputs, feat_T])
+# """
+#  We can then define a general function to build T-net layers.
+# """
 
 
-"""
-The main network can be then implemented in the same manner where the t-net mini models
-can be dropped in a layers in the graph. Here we replicate the network architecture
-published in the original paper but with half the number of weights at each layer as we
-are using the smaller 10 class ModelNet dataset.
-"""
-print("creating network...")
-inputs = keras.Input(shape=(NUM_POINTS, 3))
-print("adding tnet")
-x = tnet(inputs, 3)
-print("adding conv layers")
-x = conv_bn(x, 32)
-x = conv_bn(x, 32)
-print("adding tnet")
-x = tnet(x, 32)
-print("adding conv layers")
-x = conv_bn(x, 32)
-x = conv_bn(x, 64)
-x = conv_bn(x, 512)
-print("adding max pooling layer")
-x = layers.GlobalMaxPooling1D()(x)
-print("adding full connected and dropout layer")
-x = dense_bn(x, 256)
-x = layers.Dropout(0.3)(x)
-x = dense_bn(x, 128)
-x = layers.Dropout(0.3)(x)
+# def tnet(inputs, num_features):
 
-outputs = layers.Dense(NUM_CLASSES, activation="softmax")(x)
+#     # Initalise bias as the indentity matrix
+#     bias = keras.initializers.Constant(np.eye(num_features).flatten())
+#     reg = OrthogonalRegularizer(num_features)
+
+#     x = conv_bn(inputs, 32)
+#     x = conv_bn(x, 64)
+#     x = conv_bn(x, 512)
+#     x = layers.GlobalMaxPooling1D()(x)
+#     x = dense_bn(x, 256)
+#     x = dense_bn(x, 128)
+#     x = layers.Dense(
+#         num_features * num_features,
+#         kernel_initializer="zeros",
+#         bias_initializer=bias,
+#         activity_regularizer=reg,
+#     )(x)
+#     feat_T = layers.Reshape((num_features, num_features))(x)
+#     # Apply affine transformation to input features
+#     return layers.Dot(axes=(2, 1))([inputs, feat_T])
+
+
+# """
+# The main network can be then implemented in the same manner where the t-net mini models
+# can be dropped in a layers in the graph. Here we replicate the network architecture
+# published in the original paper but with half the number of weights at each layer as we
+# are using the smaller 10 class ModelNet dataset.
+# """
+# print("creating network...")
+# inputs = keras.Input(shape=(NUM_POINTS, 3))
+# print("adding tnet")
+# x = tnet(inputs, 3)
+# print("adding conv layers")
+# x = conv_bn(x, 32)
+# x = conv_bn(x, 32)
+# print("adding tnet")
+# x = tnet(x, 32)
+# print("adding conv layers")
+# x = conv_bn(x, 32)
+# x = conv_bn(x, 64)
+# x = conv_bn(x, 512)
+# print("adding max pooling layer")
+# x = layers.GlobalMaxPooling1D()(x)
+# print("adding full connected and dropout layer")
+# x = dense_bn(x, 256)
+# x = layers.Dropout(0.3)(x)
+# x = dense_bn(x, 128)
+# x = layers.Dropout(0.3)(x)
+
+# outputs = layers.Dense(NUM_CLASSES, activation="softmax")(x)
 
 #model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
-model = get_model()
-model.summary()
 
 """
 ### Train model
