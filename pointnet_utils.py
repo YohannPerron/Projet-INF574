feff15adb7daa98e7ed0240bac9417394a0aade8
Yohann PERRON <yohann.perron@polytechnique.edu>
2021-11-21 17:22:33 +0100

    move back sample and grouping

--- pointnet_utils.py
+++ pointnet_utils.py
@@ -1,41 +1,40 @@
+import os
+import sys
+import numpy as np
 import tensorflow as tf
-from pnet2_layers.utils import sample_and_group
-# from pnet2_layers.cpp_modules import (
-# 	farthest_point_sample,
-# 	gather_point,
-# 	query_ball_point,
-# 	group_point,
-# 	knn_point,
-# 	three_nn,
-# 	three_interpolate
-# )
 
-# def sample_and_group(npoint, radius, nsample, xyz, points):
-#     '''
-#     Input:
-#         npoint: int
-#         radius: float
-#         nsample: int
-#         xyz: (batch_size, ndataset, 3) TF tensor
-#         points: (batch_size, ndataset, channel) TF tensor, if None will just use xyz as points
-#     Output:
-#         new_xyz: (batch_size, npoint, 3) TF tensor
-#         new_points: (batch_size, npoint, nsample, 3+channel) TF tensor
-#         idx: (batch_size, npoint, nsample) TF tensor, indices of local points as in ndataset points
-#         grouped_xyz: (batch_size, npoint, nsample, 3) TF tensor, normalized point XYZs
-#             (subtracted by seed point XYZ) in local regions
-#     '''
-#     new_xyz = gather_point(xyz, farthest_point_sample(npoint, xyz))
-#     _,idx = knn_point(nsample, xyz, new_xyz)
-#     grouped_xyz = group_point(xyz, idx) # (batch_size, npoint, nsample, 3)
-#     grouped_xyz -= tf.tile(tf.expand_dims(new_xyz, 2), [1,1,nsample,1]) # translation normalization
-#     if points is not None:
-#         grouped_points = group_point(points, idx) # (batch_size, npoint, nsample, channel)
-#         new_points = tf.concat([grouped_xyz, grouped_points], axis=-1) # (batch_size, npoint, nample, 3+channel)
-#     else:
-#         new_points = grouped_xyz
-#     return new_xyz, new_points, idx, grouped_xyz
+from tensorflow.keras.layers import MaxPool1D, Layer, BatchNormalization
 
+from .cpp_modules import (
+	farthest_point_sample,
+	gather_point,
+	query_ball_point,
+	group_point,
+	knn_point,
+	three_nn,
+	three_interpolate
+)
+
+
+def sample_and_group(npoint, radius, nsample, xyz, points, knn=False, use_xyz=True):
+
+	new_xyz = gather_point(xyz, farthest_point_sample(npoint, xyz)) # (batch_size, npoint, 3)
+	if knn:
+		_,idx = knn_point(nsample, xyz, new_xyz)
+	else:
+		idx, pts_cnt = query_ball_point(radius, nsample, xyz, new_xyz)
+	grouped_xyz = group_point(xyz, idx) # (batch_size, npoint, nsample, 3)
+	grouped_xyz -= tf.tile(tf.expand_dims(new_xyz, 2), [1,1,nsample,1]) # translation normalization
+	if points is not None:
+		grouped_points = group_point(points, idx) # (batch_size, npoint, nsample, channel)
+		if use_xyz:
+			new_points = tf.concat([grouped_xyz, grouped_points], axis=-1) # (batch_size, npoint, nample, 3+channel)
+		else:
+			new_points = grouped_points
+	else:
+		new_points = grouped_xyz
+
+	return new_xyz, new_points, idx, grouped_xyz
 
 def pointnet_downsample(xyz, points, npoint, radius, nsample, NN, NN2 = None):
     ''' PointNet Set Abstraction (SA) Module
