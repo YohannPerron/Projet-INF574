11693dc97784e627b3a160f2142b77dec10db77d
Yohann PERRON <yohann.perron@polytechnique.edu>
2021-11-26 16:29:56 +0100

    corrected imports

--- models/pointnet2_part_seg_msg.py
+++ models/pointnet2_part_seg_msg.py
@@ -5,7 +5,7 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
-from models.pointnet2_utils import PointNetSetAbstractionMsg,PointNetSetAbstraction,PointNetFeaturePropagation
+from models.pointnet2_layers import PointNetSetAbstractionMsg,PointNetSetAbstraction,PointNetFeaturePropagation
 
 
 class get_model(nn.Module):
@@ -19,9 +19,9 @@ class get_model(nn.Module):
         self.sa1 = PointNetSetAbstractionMsg(512, [0.1, 0.2, 0.4], [32, 64, 128], 3+additional_channel, [[32, 32, 64], [64, 64, 128], [64, 96, 128]])
         self.sa2 = PointNetSetAbstractionMsg(128, [0.4,0.8], [64, 128], 128+128+64, [[128, 128, 256], [128, 196, 256]])
         self.sa3 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, input_size=512 + 3, NNlayers=[256, 512, 1024], group_all=True)
-        self.fp3 = PointNetFeaturePropagation(in_channel=1536, mlp=[256, 256])
-        self.fp2 = PointNetFeaturePropagation(in_channel=576, mlp=[256, 128])
-        self.fp1 = PointNetFeaturePropagation(in_channel=150+additional_channel, mlp=[128, 128])
+        self.fp3 = PointNetFeaturePropagation(input_size=1536, NNLayers=[256, 256])
+        self.fp2 = PointNetFeaturePropagation(input_size=576, NNLayers=[256, 128])
+        self.fp1 = PointNetFeaturePropagation(input_size=150+additional_channel, NNLayers=[128, 128])
         self.conv1 = nn.Conv1d(128, 128, 1)
         self.bn1 = nn.BatchNorm1d(128)
         self.drop1 = nn.Dropout(0.5)
