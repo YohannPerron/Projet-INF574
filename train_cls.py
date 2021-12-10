ee59ae4f3b726271029c2620ab0f1c6090c091b1
georgiohelou <42733892+georgiohelou@users.noreply.github.com>
2021-12-10 18:42:07 +0100

    Update train_cls.py

--- train_cls.py
+++ train_cls.py
@@ -2,15 +2,11 @@ import os
 import glob
 import trimesh
 import numpy as np
-from matplotlib import pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
 from pointnet2_class import get_model
-import models.cls_msg_model as model
-
-# from tensorflow.python.framework.ops import disable_eager_execution
-# disable_eager_execution()
-
 
 tf.random.set_seed(1234)
 
@@ -20,11 +16,8 @@ First download the file
 P.S: change ModelNet10 to ModelNet40 to get same results as paper
 """
 
-DATA_DIR = tf.keras.utils.get_file(
-    "modelnet.zip",
-    "http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip",
-    extract=True,
-)
+#Downlaod file and extract zip
+DATA_DIR = tf.keras.utils.get_file("modelnet.zip","http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip",extract=True,)
 DATA_DIR = os.path.join(os.path.dirname(DATA_DIR), "ModelNet10")
 
 
@@ -40,86 +33,193 @@ The folder name is the label; we store it and add it the dict
 """
 
 
-def parse_dataset(num_points=2048):
+def parse_dataset():
 
-    train_points = []
-    train_labels = []
-    test_points = []
-    test_labels = []
-    class_map = {}
-    folders = glob.glob(os.path.join(DATA_DIR, "[!README]*"))
-
-    for i, folder in enumerate(folders):
-        print("processing class: {}".format(os.path.basename(folder)))
+    #create lsits to store train/test points and their relative labels
+    trainPts = []
+    trainLbl = []
+    testPts = []
+    testLbl = []
+    CLASS_MAP = {}
+  
+    Directories = glob.glob(os.path.join(DATA_DIR, "[!README]*"))
+    for lbl, Dir in enumerate(Directories):
         # store folder name with ID so we can retrieve later
-        class_map[i] = folder.split("/")[-1]
+        CLASS_MAP[lbl] = Dir.split("/")[-1]
 
-        # gather all files
-        train_files = glob.glob(os.path.join(folder, "train/*"))
-        test_files = glob.glob(os.path.join(folder, "test/*"))
+        # gather all files inside of train and test folders
+        # and classify them according to their use
+        trainAll = glob.glob(os.path.join(Dir, "train/*"))
+        testAll = glob.glob(os.path.join(Dir, "test/*"))
 
-        for f in train_files:
-            train_points.append(trimesh.load(f).sample(num_points))
-            train_labels.append(i)
+        #iterate over all files and populare lists accordingly
+        for f in trainAll:
+            trainLbl.append(lbl)
+            trainPts.append(trimesh.load(f).sample(2048))
 
-        for f in test_files:
-            test_points.append(trimesh.load(f).sample(num_points))
-            test_labels.append(i)
+        for f in testAll:
+            testLbl.append(lbl)
+            testPts.append(trimesh.load(f).sample(2048))
 
-    return (
-        np.array(train_points),
-        np.array(test_points),
-        np.array(train_labels),
-        np.array(test_labels),
-        class_map,
-    )
+    #convert list to np.array
+    TRAINPTS=np.array(trainPts)
+    TESTPTS=np.array(testPts)
+    TRAINLABEL=np.array(trainLbl)
+    TESTLABEL=np.array(testLbl)
+    return (TRAINPTS,TESTPTS,TRAINLABEL,TESTLABEL,CLASS_MAP,)
 
 
-"""
-Set the number of points to sample and batch size and parse the dataset.
-P.S: change the number of classes to 40 for modelNet40
-"""
-
-NUM_POINTS = 2048
-NUM_CLASSES = 10
-BATCH_SIZE = 32
-
-# model = get_model(NUM_POINTS)
-model = model.CLS_MSG_Model(BATCH_SIZE,NUM_CLASSES)
-# model.summary()
 
 """
 Parse data in tf.data.Dataset()
 """
 print("parsing data ...")
-train_points, test_points, train_labels, test_labels, CLASS_MAP = parse_dataset(
-    NUM_POINTS
-)
+TRAINPTS, TESTPTS, TRAINLABEL, TESTLABEL, CLASS_MAP = parse_dataset()
 
 
 """
-Augmentation includes shuffling and jittering 
+shuffling and adding noise 
 """
-def augment(points, label):
-    # jitter points
-    points += tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
-    # shuffle points
+def alter(points, label):
+    # adding noise
+    points = points + tf.random.uniform(points.shape, -0.005, 0.005, dtype=tf.float64)
+    # shuffling
     points = tf.random.shuffle(points)
     return points, label
 
 """
 We shuffle the dataset as it was previously ordered by class
-And we apply data augmentation
+And we apply noise and shuffling
 """
 print("setting up training and testing datasets...")
-train_dataset = tf.data.Dataset.from_tensor_slices((train_points, train_labels))
-test_dataset = tf.data.Dataset.from_tensor_slices((test_points, test_labels))
+train_dataset = tf.data.Dataset.from_tensor_slices((TRAINPTS, TRAINLABEL))
+test_dataset = tf.data.Dataset.from_tensor_slices((TESTPTS, TESTLABEL))
 
-train_dataset = train_dataset.shuffle(len(train_points)).map(augment).batch(BATCH_SIZE)
-test_dataset = test_dataset.shuffle(len(test_points)).batch(BATCH_SIZE)
+#use batch size of 32
+train_dataset = train_dataset.shuffle(len(TRAINPTS)).map(alter).batch(32)
+test_dataset = test_dataset.shuffle(len(TESTPTS)).batch(32)
 
+"""
+### Build a model
+Each convolution and fully-connected layer (with exception for end layers) consits of
+Convolution / Dense -> Batch Normalization -> ReLU Activation.
+"""
+
+
+def conv_bn(x, filters):
+    x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
+    x = layers.BatchNormalization(momentum=0.0)(x)
+    return layers.Activation("relu")(x)
+
+
+def dense_bn(x, filters):
+    x = layers.Dense(filters)(x)
+    x = layers.BatchNormalization(momentum=0.0)(x)
+    return layers.Activation("relu")(x)
+
+
+"""
+PointNet consists of two core components. The primary MLP network, and the transformer
+net (T-net). The T-net aims to learn an affine transformation matrix by its own mini
+network. The T-net is used twice. The first time to transform the input features (n, 3)
+into a canonical representation. The second is an affine transformation for alignment in
+feature space (n, 3). As per the original paper we constrain the transformation to be
+close to an orthogonal matrix (i.e. ||X*X^T - I|| = 0).
+"""
+
+
+class OrthogonalRegularizer(keras.regularizers.Regularizer):
+    def __init__(self, num_features, l2reg=0.001):
+        self.num_features = num_features
+        self.l2reg = l2reg
+        self.eye = tf.eye(num_features)
+
+    def __call__(self, x):
+        x = tf.reshape(x, (-1, self.num_features, self.num_features))
+        xxt = tf.tensordot(x, x, axes=(2, 2))
+        xxt = tf.reshape(xxt, (-1, self.num_features, self.num_features))
+        return tf.reduce_sum(self.l2reg * tf.square(xxt - self.eye))
+
+
+"""
+ We can then define a general function to build T-net layers.
+"""
+
+
+def tnet(inputs, num_features):
+
+    # Initalise bias as the indentity matrix
+    bias = keras.initializers.Constant(np.eye(num_features).flatten())
+    reg = OrthogonalRegularizer(num_features)
+
+    x = conv_bn(inputs, 32)
+    x = conv_bn(x, 64)
+    x = conv_bn(x, 512)
+    x = layers.GlobalMaxPooling1D()(x)
+    x = dense_bn(x, 256)
+    x = dense_bn(x, 128)
+    x = layers.Dense(
+        num_features * num_features,
+        kernel_initializer="zeros",
+        bias_initializer=bias,
+        activity_regularizer=reg,
+    )(x)
+    feat_T = layers.Reshape((num_features, num_features))(x)
+    # Apply affine transformation to input features
+    return layers.Dot(axes=(2, 1))([inputs, feat_T])
+
+
+"""
+The main network can be then implemented in the same manner where the t-net mini models
+can be dropped in a layers in the graph. Here we replicate the network architecture
+published in the original paper but with half the number of weights at each layer as we
+are using the smaller 10 class ModelNet dataset.
+"""
+print("creating network...")
+inputs = keras.Input(shape=(2048, 3))
+print("adding tnet")
+x = tnet(inputs, 3)
+print("adding conv layers")
+
+x = layers.Conv1D(32, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Conv1D(32, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+print("adding tnet")
+x = tnet(x, 32)
+print("adding conv layers")
+x = layers.Conv1D(32, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Conv1D(64, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+x = layers.Conv1D(512, kernel_size=1, padding="valid")(x)
+x = layers.BatchNormalization(momentum=0.0)(x)
+x = layers.Activation("relu")(x)
+
+print("adding max pooling layer")
+x = layers.GlobalMaxPooling1D()(x)
+
+print("adding full connected and dropout layer")
+x = dense_bn(x, 256)
+x = layers.Dropout(0.3)(x)
+x = dense_bn(x, 128)
+x = layers.Dropout(0.3)(x)
+
+#we have 10 classes 
+#P.S: change to 40 to get same results
+outputs = layers.Dense(10, activation="softmax")(x)
 
 #model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
+model = get_model()
+model.summary()
 
 """
 ### Train model
@@ -128,7 +228,7 @@ using `.compile()` and `.fit()`.
 """
 print("compiling model")
 model.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(),
+    loss="sparse_categorical_crossentropy",
     optimizer=keras.optimizers.Adam(learning_rate=0.001),
     metrics=["sparse_categorical_accuracy"],
 )
